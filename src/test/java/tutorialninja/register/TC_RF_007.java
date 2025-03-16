78315882b1e890dad293e4a26388cc552b328a2e
Manikanta <manikantaqaautomation@gmail.com>
2025-03-16 10:43:18 +0530

    TC_RF_007

---
+++ src/test/java/tutorialninja/register/TC_RF_007.java
@@ -0,0 +1,46 @@
+package tutorialninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_007 {
+	@Test
+	public void verifyRegisteringAccountNavigateToMultipleWays(){
+		
+		WebDriver  driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		//Open the URL
+		driver.get("https://tutorialsninja.com/demo/");
+		//Test Step-1
+		//Click on 'My Account' drop down menu
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		//Select the option from 'My Account' drop down menu
+		driver.findElement(By.xpath("//a[text()='Register']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+		//Click on 'My Account' drop down menu
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		//Select the option from 'Login' drop down menu
+		driver.findElement(By.linkText("Login")).click();
+		//Click on continue button for Registering new Account
+		driver.findElement(By.xpath("//a[@class='btn btn-primary'][text()='Continue']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+		//Click on 'My Account' drop down menu
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		//Select the option from 'Login' drop down menu
+		driver.findElement(By.linkText("Login")).click();
+		//Select the option as 'Register' from Right menu options
+		driver.findElement(By.xpath("//a[@class='list-group-item'][text()='Register']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Register']")).isDisplayed());
+		
+		
+		
+		
+	}
+
+}
