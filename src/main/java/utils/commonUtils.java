9ce07fb06f55f6fcade2c7e2bf6fec9891d738d6
Manikanta <manikantaqaautomation@gmail.com>
2025-03-28 17:14:47 +0530

    TC_RF_018

--- src/main/java/utils/commonUtils.java
+++ src/main/java/utils/commonUtils.java
@@ -1,6 +1,14 @@
 package utils;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+import ru.yandex.qatools.ashot.comparison.ImageDiff;
+import ru.yandex.qatools.ashot.comparison.ImageDiffer;
 
 public class commonUtils {
 	
@@ -9,5 +17,22 @@ public static String generateEmail() {
 		return new Date().toString().replaceAll("\\s","").replaceAll("\\:","")+"@mail.com";
 		
 	}
+
+public static boolean compareTwoScreenshots(String actualImagePath, String expectedImagePath) {
+	BufferedImage acutualBImg = null;
+	BufferedImage expectedBImg = null;
+	try {
+		acutualBImg = ImageIO.read(new File(System.getProperty("user.dir")+actualImagePath));
+		expectedBImg = ImageIO.read(new File(System.getProperty("user.dir")+expectedImagePath));
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+
+	ImageDiffer imgDiffer = new ImageDiffer();
+	ImageDiff imgDifference = imgDiffer.makeDiff(expectedBImg, acutualBImg);
+
+	return imgDifference.hasDiff();
+
+}
 
 }
